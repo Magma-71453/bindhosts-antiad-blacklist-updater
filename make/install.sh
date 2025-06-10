9fac76e306834cb44cdf5e705e2b47c34e25c69b
Magma <zeshimengxin71453@gmail.com>
2025-06-10 23:38:17 +0800

    Create install.sh

---
+++ make/install.sh
@@ -0,0 +1,14 @@
+SKIPMOUNT=false
+LATESTARTSERVICE=true
+POSTFSDATA=false
+PROPFILE=false
+on_install() {
+ unzip -o "$ZIPFILE" 'action.sh' -d $MODPATH >&2
+ unzip -o "$ZIPFILE" 'curl' -d $MODPATH >&2
+ unzip -o "$ZIPFILE" 'rename' -d $MODPATH >&2
+ unzip -o "$ZIPFILE" 'service.sh' -d $MODPATH >&2
+}
+set_permissions() {
+ set_perm_recursive $MODPATH 0 0 0755 0755
+#设置权限，基本不要去动
+}
